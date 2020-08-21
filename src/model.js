d44b571b288bb36f93277929f12f0a17992bbfe2
Andrei Hincu <a.hincu@octopod.ru>
2020-08-21 14:24:52 +0300

    2 day
    add model

---
+++ src/model.js
@@ -0,0 +1,11 @@
+export const model = [
+    {type: 'title', value: 'Test Title', options: {}},
+    {type: 'text', value: 'text text text text text text text text text text text text text text text ', options: {}},
+    {type: 'textColumns', value: [
+            '1text',
+            '2 text',
+            '3 text',
+            '4 text'
+        ]
+        , options: {}},
+]
